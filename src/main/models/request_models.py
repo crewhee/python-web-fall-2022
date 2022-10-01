4d8d25c82bc5afeb6d69e688b9dc0f865c1d4a01
Crewhee <tony.sudov@gmail.com>
2022-10-01 21:14:21 +0300

    Clear directories

--- src/main/models/request_models.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-from pydantic import BaseModel
-
-
-class RequestBody(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    some_number: Union[int, None] = None
